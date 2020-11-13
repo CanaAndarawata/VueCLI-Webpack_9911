74b4af07cc9ece03ef21c7e8f54458f3fcfcf855
CanaAndarawata <Madecana70@gmail.com>
2020-11-13 10:16:53 +0800

    UGD Vue CLI

--- src/components/TodoList/List.vue
+++ src/components/TodoList/List.vue
@@ -31,6 +31,7 @@
     </v-data-table>
  </v-card>
 
+\\dialog
  <v-dialog v-model="dialog" persistent max-width="600px">
  <v-card>
     <v-card-title>
@@ -67,6 +68,7 @@
     </v-card-actions>
  </v-card>
  </v-dialog>
+
  </v-main>
 </template>
 <script>
